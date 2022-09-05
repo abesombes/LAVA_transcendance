69ab5b4b6cf2f07ab5d5bf2625518941b2f0b33c
Antoine EDOUARD <jhown@antoines-mbp.home>
2022-09-05 05:04:44 +0200

    set a proxy for the nestjs container

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+  // transpileDependencies: true,
+  module.exports = {
+    devServer: {
+      proxy: {
+        // proxy to bypass CORS
+        // rewrite url as http://localhost:8080/... into http://localhost:3000/...
+        "^/api": {
+          target: "http://nestjs:3000/",
+          changeOrigin: true,
+          secure: false,
+          pathRewrite: {'^/api': '/api'},
+          logLevel: 'debug',
+        }
+      }
+    }
+  }
