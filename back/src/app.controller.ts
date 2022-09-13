19d61f85dcfa8780fce5294f8b6f2f57a7a64daa
abesombe <abesombe@student.42.fr>
2022-09-13 23:25:13 +0200

    Prisma added in NestJs container + sample CRUD

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -1,13 +1,57 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Delete } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+//email: string, name: string
+
+  @Post('api/users/create')
+  async createUser(@Body() body): Promise<any> {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email, 
+        name: body.name,
+        },
+      })
+}
+
+
+  @Get('api/fetch-users')
+  async fetchUsers(): Promise<any> {
+    const users = await prisma.user.findMany()
+    return (users);
+  }
+
+  @Get('api/fetch-user/:id')
+  async fetchUser(@Param('id') id: string): Promise<any> {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: parseInt(id), // or Number(id)
+      },
+    })
+    return (user);
+  }
+
+  @Delete('api/delete-user/:id')
+  async deleteUser(@Param('id') id: string): Promise<any> {
+    const user = await prisma.user.delete({
+      where: {
+        id: parseInt(id), // or Number(id)
+      },
+    })
+    return (user);
+  }
 
   @Get('api')
   getHello(): any {
     return this.appService.getHello();
   }
+
   @Get()
   getRootRoute(){
     return 'hi there!';
--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [],
